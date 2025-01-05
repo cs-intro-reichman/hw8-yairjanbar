2041e51a340695100787c9811278f7da676f008a
yairjanbar <yair.janbar@gmail.com>
2025-01-06 01:36:49 +0200

    testing the follows fun after adding first letter

--- User.java
+++ User.java
@@ -44,8 +44,13 @@
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
         //// Replace the following statement with your code
+        char firstL = name.charAt(0);
+        if (firstL >= 'a' && firstL <= 'z') {
+            firstL = (char) (firstL - 32);
+        }
+        String newName = firstL + name.substring(1);
         for (int i=0; i<follows.length;i++){
-            if (follows[i] != null && follows[i].equals(name)) {
+            if (follows[i] != null && follows[i].equals(newName)) {
                 return true;
             }
         }
@@ -55,6 +60,7 @@
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
         //// Replace the following statement with your code
+        if 
         return false;
     }
 
