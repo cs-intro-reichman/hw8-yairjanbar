a44b3516a291dc9481f163a9ef7318ae3e2d72d2
yairjanbar <yair.janbar@gmail.com>
2025-01-06 04:56:44 +0200

    fixed tostr

--- Network.java
+++ Network.java
@@ -178,12 +178,12 @@ public class Network {
        String result = "Network:";
        for (int i = 0; i < userCount; i++) {
         String followees = users[i].toString();
-        if (!followees.equals("")) {
+        if (followees != null && !followees.isEmpty()) {
             result += "\n" + followees + " ->";
         } else {
             result += "\n" + followees;
+        } 
         }
-    }
        return result;
     }
 }
